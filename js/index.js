2b68d850acd879a710cae0f49f8bfd0daf2f3649
unknown <ma7838877@gmail.commoh12..eygit config --global user.name>
2022-05-15 21:37:41 +0200

    our team modified

--- js/index.js
+++ js/index.js
@@ -6,7 +6,7 @@ $(document).ready(function(){
             autoplay: true,
             autoplaySpeed: 900,
             infinite: true,
-            centerMode: true,
+            centerMode: false,
             responsive: [
                 {
                   breakpoint: 991,
@@ -37,5 +37,27 @@ $(document).ready(function(){
     $("#btn").click(function() {
         window.scrollTo({ top: 550, behavior: 'smooth'});
     });
+
+    if(pageYOffset <= 665){
+        $(".navbar").removeClass("second_navbar");
+        $(".navbar").addClass("first_nav");
+    }
+    else{
+        $(".navbar").addClass("second_navbar");
+        $(".navbar").removeClass("first_nav");
+      }
+      
+      
+        $(window).on("scroll", function () {
+      
+          if(pageYOffset >= 665){
+            $(".navbar").addClass("second_navbar");
+            $(".navbar").removeClass("first_nav");
+          }else{
+            $(".navbar").removeClass("second_navbar");
+            $(".navbar").addClass("first_nav");
+          }
+        });
+      
 });
 
