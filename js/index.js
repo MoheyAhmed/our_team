4d9c1b780804474fb80c0b125947bda19aece322
unknown <ma7838877@gmail.commoh12..eygit config --global user.name>
2022-05-15 20:58:27 +0200

    our team after modify

--- js/index.js
+++ js/index.js
@@ -1,13 +1,38 @@
 $(document).ready(function(){
     
-    $('.slickSlider').slick({
-        slidesToShow: 4,
-        slidesToScroll: 1,
-        autoplay: true,
-        autoplaySpeed: 900,
-        infinite: true,
-        centerMode: true,
-    });
+        $('.slickSlider').slick({
+            slidesToShow: 4,
+            slidesToScroll: 1,
+            autoplay: true,
+            autoplaySpeed: 900,
+            infinite: true,
+            centerMode: true,
+            responsive: [
+                {
+                  breakpoint: 991,
+                  settings: {
+                    slidesToShow: 3,
+                  },
+                },
+                {
+                  breakpoint: 767,
+                  settings: {
+                    slidesToShow: 2,
+                    centerMode: false,
+                  },
+                },
+                {
+                  breakpoint: 450,
+                  settings: {
+                    slidesToShow: 1,
+                    centerMode: false,
+                  },
+                },
+                
+            ],
+        });
+
+
     
     $("#btn").click(function() {
         window.scrollTo({ top: 550, behavior: 'smooth'});
